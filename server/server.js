59f93de092a88ad76e26963ba63545696882edff
ultragrace <udechukwuakachukwu.com>
2020-05-01 23:36:57 +0100

    Add PATH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 const express = require('express');
-var bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
 var {mongoose} = require('./db/mongos');
 var {Todo} = require('./models/todos');
@@ -74,6 +75,36 @@ app.delete('/todos/:id', (req, res) => {
     });
 });
 
+
+app.patch('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send()
+     }
+
+     if (_.isBoolean(body.completed) && body.completed) {
+         body.completedAt = new Date().getTime();
+     }else{
+         body.completed = false;
+         body.completedAt = null;
+     }
+ 
+    //  query
+    Todo.findByIdAndUpdate(id, {
+        $set: body
+    }, {
+        new: true
+    }).then((todo) => {
+        if(!todo){
+            res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) =>{
+        res.status(400).send();
+    })
+})
 
 
 app.listen(port, ()=>{
